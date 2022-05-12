3339f0873356c941342595f851948ac153ec8566
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-05-12 18:01:40 +0200

    a bunch of stuff
    General improvments
    Completed GetEvent and IsChoicePossible method and started handeling endings

--- Text-based-game/Program.cs
+++ Text-based-game/Program.cs
@@ -5,11 +5,20 @@ using System.Text.RegularExpressions;
 
 namespace Text_based_game
 {
+    class End
+    {
+        public string Name;
+        public string Narration;
+        public Event EventRequirement;
+        public int MaxConfidence;
+        public int MinConfidence;
+        public int MinAlarmLevel;
+        public int MaxAlarmLevel;
+    }
     class Event
     {
         public string Name;
         public string Narration;
-        public string SpecialItem;
         public List<Choice> Choices;
     }
     class Choice
@@ -17,10 +26,12 @@ namespace Text_based_game
         public int MinimumConfidence;
         public int ConfidenceAlteration;
         public int AlarmLevelAlteration;
-        public Event EventRequirement;
+        public string EventRequirement;
         public string SpecialItemRequirement;
+        public string SpecialItemGained;
         public string Narration;
         public string Name;
+        public string NextEvent;
     }
     [Flags]
     public enum ConditionCheck
@@ -34,8 +45,29 @@ namespace Text_based_game
     }
     internal class Program
     {
-        //Checks conditions for choices, endings and rules.
-        static bool isChoicePosible(Choice choice, int confidence, List<Event> storyline, List<string> inventory)
+        static List<Event> events = new List<Event>();
+
+        //A method which checks if the game is over and the ending the player got if the game is over.
+        static End handleEndings(int confidence, int alarmLevel)
+        {
+
+        }
+
+        //TODO:EncounteringTheDragon method
+
+
+        //Gets the next event.
+        static Event GetEvent(string name)
+        {
+            Event result = events.Find(storyEvent => storyEvent.Name == name);
+            if (result != null)
+            {
+                return result;
+            }
+            throw new ArgumentException("Event does not exist");
+        }
+        //Checks conditions for the choices of the current event.
+        static bool isChoicePossible(Choice choice, int confidence, List<string> storyline, List<string> inventory)
         {
             if (confidence < choice.MinimumConfidence)
             {
@@ -56,87 +88,154 @@ namespace Text_based_game
         }
         static void Main(string[] args)
         {
-            int confidence = 1;
-            int alarmLevel = 0;
-            string userInput;
-            int userInputInt;
-
-            //List of events the player has cleared.
-            var storyline = new List<Event>();
-            //List of special item player has gained.
-            var inventory = new List<string>();
-            //List of choices in individual events.
-            var eventChoices = new List<Choice>();
-
+            //Inizalasation
             //Reading files and then splitting them.
             string eventsPath = "Events.txt";
             string eventsText = File.ReadAllText(eventsPath);
-            string[] eventGroups = eventsText.Split("\r\n\r");
+            string[] eventGroups = eventsText.Split("\r\n\r\n");
 
             string choicesPath = "Choices.txt";
             string choicesText = File.ReadAllText(choicesPath);
             string[] choiceGroups = choicesText.Split("\r\n\r\n\r\n");
 
-            //Game intro.
-            Console.WriteLine("A lone thief has set up camp in a forest. Less than a day away has a dragon made its lair.\nThe thief has gathered what confidence they could find and has made it this far...\nWill their confidence grow or fallter? Will the dragon spot them or will they go unseen...\nOnly time will tell...");
-            Console.WriteLine("\nPress any button to commence with the theft.");
-            userInput = Console.ReadLine();
 
-            int e = 0;
-            int c = 0;
 
-            do
+            for (int eventIndex = 0; eventIndex < eventGroups.Length; eventIndex++)
             {
-                //Using regex to find individual items in the events file.
-                Match eventInfo = Regex.Match(eventGroups[e], ".*:(.*)\\n.*:(.*)\\n.*:(.*)");
+                //List of choices in individual events.
+                var eventChoices = new List<Choice>();
+                string[] choicesPerEvent = choiceGroups[eventIndex].Split("\r\n\r\n");
 
                 //Using regex to find individual items in the choice file.
 
-                string[] choicesPerEvent = choiceGroups[c].Split("\r\n\r");
-                string choicesPerEventString = String.Concat(choicesPerEvent);
-
-                MatchCollection choiceInfoCollection = Regex.Matches(choicesPerEventString, "(\\d\\..*)\\n.*:(.*)\\n.*:(.*)\\n.*:(.*)\\n.*:(.*)\\n.*:(.*)\\n.*:(.*)");
-
-                foreach (Match choiceInfo in choiceInfoCollection)
+                foreach (string choiceInfoText in choicesPerEvent)
                 {
-                    GroupCollection group = choiceInfo.Groups;
-
-                    //Convert numerical string into int.
-                    int intConfidenceAlteration = Int32.Parse(choiceInfo.Groups[3].Value);
-                    int intAlarmLevelAlteration = Int32.Parse(choiceInfo.Groups[4].Value);
-                    int intMinimumConfidence = Int32.Parse(choiceInfo.Groups[5].Value);
+                    Match choiceInfo = Regex.Match(choiceInfoText, "Name: (.*)\\r\\nNarration: (.*)");
 
                     //Choice class definition.
                     var choice = new Choice();
                     choice.Name = choiceInfo.Groups[1].Value;
                     choice.Narration = choiceInfo.Groups[2].Value;
-                    choice.ConfidenceAlteration = intConfidenceAlteration;
-                    choice.AlarmLevelAlteration = intAlarmLevelAlteration;
-                    choice.MinimumConfidence = intMinimumConfidence;
-                    choice.SpecialItemRequirement = choiceInfo.Groups[6].Value;
-                    choice.EventRequirement = choiceInfo.Groups[7].Value;
+
+                    Match confidenceInfo = Regex.Match(choiceInfoText, "Confidence alteration: ([^\\r]*)");
+                    if (confidenceInfo.Success)
+                    {
+                        int intConfidenceAlteration = Int32.Parse(confidenceInfo.Groups[1].Value);
+                        choice.ConfidenceAlteration = intConfidenceAlteration;
+                    }
+
+                    Match alarmInfo = Regex.Match(choiceInfoText, "Alarm level alteration: ([^\\r]*)");
+                    if (alarmInfo.Success)
+                    {
+                        int intAlarmLevelAlteration = Int32.Parse(alarmInfo.Groups[1].Value);
+                        choice.AlarmLevelAlteration = intAlarmLevelAlteration;
+                    }
+
+                    Match minimumConfidenceInfo = Regex.Match(choiceInfoText, "Minimum confidence: ([^\\r]*)");
+                    if (minimumConfidenceInfo.Success)
+                    {
+                        int intMinimumConfidence = Int32.Parse(minimumConfidenceInfo.Groups[1].Value);
+                        choice.MinimumConfidence = intMinimumConfidence;
+                    }
+
+                    Match specialItemRequirementInfo = Regex.Match(choiceInfoText, "Special item requirement: ([^\\r]*)");
+                    if (specialItemRequirementInfo.Success)
+                    {
+                        choice.SpecialItemRequirement = specialItemRequirementInfo.Groups[1].Value;
+                    }
+
+                    Match specialItemGainedInfo = Regex.Match(choiceInfoText, "Special item gained: ([^\\r]*)");
+                    if (specialItemGainedInfo.Success)
+                    {
+                        choice.SpecialItemGained = specialItemGainedInfo.Groups[1].Value;
+                    }
+
+                    Match eventRequirementInfo = Regex.Match(choiceInfoText, "Event requriement: ([^\\r]*)");
+                    if (eventRequirementInfo.Success)
+                    {
+                        choice.EventRequirement = eventRequirementInfo.Groups[1].Value;
+                    }
+
+                    Match nextEventInfo = Regex.Match(choiceInfoText, "Next event: ([^\\r]*)");
+                    if (nextEventInfo.Success)
+                    {
+                        choice.NextEvent = nextEventInfo.Groups[1].Value;
+                    }
+
+
 
                     //Adding choices into a list that goes to event class.
                     eventChoices.Add(choice);
+
                 }
+
+                //Using regex to find individual items in the events file.
+                Match eventInfo = Regex.Match(eventGroups[eventIndex], "Name: (.*)\\r\\nNarration: (.*)");
 
                 //Event class definition.
                 var newEvent = new Event();
                 newEvent.Name = eventInfo.Groups[1].Value;
                 newEvent.Narration = eventInfo.Groups[2].Value;
-                newEvent.SpecialItem = eventInfo.Groups[3].Value;
                 newEvent.Choices = eventChoices;
+                events.Add(newEvent);
+            }
+
+
+
+            //Gameplay
+            int confidence = 1;
+            int alarmLevel = 0;
+            string userInput;
+            int intUserInput;
+
+            //List of events the player has cleared.
+            var storyline = new List<string>();
+            //List of special item player has gained.
+            var inventory = new List<string>();
+
+
+            //Game intro.
+            Console.WriteLine("A lone thief has set up camp in a forest. Less than a day away has a dragon made its lair.\nThe thief has gathered what confidence they could find and has made it this far...\nWill their confidence grow or fallter? Will the dragon spot them or will they go unseen...\nOnly time will tell...");
+            Console.WriteLine("\nPress any button to commence with the theft.");
+            Console.ReadLine();
+
+            Event currentEvent = events[0];
+
+            do
+            {
+                //Alarm check
+                if (alarmLevel == 5)
+                {
+                    //TODO:Move to encounteringTheDragon method
+                }
+
+                //TODO:Check ending
+
 
                 //Clearing previous text and output UI.
                 Console.Clear();
                 Console.WriteLine($"Confidence:{confidence} Alarm Level:{alarmLevel}\n");
-                Console.WriteLine(newEvent.Narration);
-                foreach (Choice choice in eventChoices)
+                Console.WriteLine(currentEvent.Narration);
+                var possibleChoices = new List<Choice>();
+                foreach (Choice choice in currentEvent.Choices)
                 {
-                    Console.WriteLine(choice.Name);
+                    if (isChoicePossible(choice, confidence, storyline, inventory))
+                    {
+                        possibleChoices.Add(choice);
+                    }
+                }
+                int counter = 1;
+                foreach (Choice choice in possibleChoices)
+                {
+                    Console.WriteLine($"{counter}. {choice.Name}");
+                    counter++;
                 }
 
+
+
                 //Selects a choice based on the players input.
+                Choice selectedChoice;
+
                 do
                 {
                     userInput = Console.ReadLine();
@@ -145,12 +244,9 @@ namespace Text_based_game
                     {
                         try
                         {
-                            userInputInt = int.Parse(userInput);
-                            Console.WriteLine(eventChoices[userInputInt - 1].Narration);
+                            intUserInput = int.Parse(userInput);
+                            selectedChoice = possibleChoices[intUserInput - 1];
 
-                            confidence += eventChoices[userInputInt - 1].ConfidenceAlteration;
-                            alarmLevel += eventChoices[userInputInt - 1].AlarmLevelAlteration;
-                            Console.ReadLine();
                             break;
                         }
                         catch (ArgumentOutOfRangeException)
@@ -165,20 +261,46 @@ namespace Text_based_game
 
                 } while (true);
 
+                //Display choice
+                Console.WriteLine(selectedChoice.Narration);
+                Console.ReadLine();
+
+                confidence += selectedChoice.ConfidenceAlteration;
+                alarmLevel += selectedChoice.AlarmLevelAlteration;
+
                 //Adds found item to inventory.
-                if (Regex.IsMatch(newEvent.Name, "Knights camp") && (userInput == "1"))
+                if (selectedChoice.SpecialItemGained != null)
                 {
-                    inventory.Add(newEvent.SpecialItem);
+                    inventory.Add(selectedChoice.SpecialItemGained);
                 }
 
-                //Moves to next event.
-                eventChoices.Clear();
-                storyline.Add(newEvent);
-                e++;
-                c++;
+                storyline.Add(currentEvent.Name);
+
+                //Move to next event
+                if (currentEvent.Name == "Knights camp")
+                {
+                    if (inventory.Contains("Main entrance"))
+                    {
+
+                        currentEvent = GetEvent("The main entrance");
+                    }
+                    else if (inventory.Contains("Underground tunnels"))
+                    {
+                        currentEvent = GetEvent("The underground tunnel");
+                    }
+                    else
+                    {
+                        currentEvent = GetEvent("The secret door");
+                    }
+                }
+                else
+                {
+                    currentEvent = GetEvent(selectedChoice.NextEvent);
+                }
 
 
-            } while (e < 3);
+
+            } while (true);
 
         }
     }
